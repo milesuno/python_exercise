2429bb1987382b62b65dda2c06ed9a942ce2169e
milesuno <milesoluku@gmail.com>
2020-01-09 17:08:08 +0000

    Refactor and debugged

--- python_exercises/car_game.py
+++ python_exercises/car_game.py
@@ -1,43 +1,45 @@
 # Working with while loops and conditional statements
-exit = False
-car_on = False
 
-while exit == False:
-    user_input = input("Enter command: ").upper()
-    print(f"user entered{user_input}")
 
-    if user_input == "HELP":
-        print(
-            """
+def car_game():
+    game_exit = False
+    car_on = False
+
+    while not game_exit:
+        user_input = input("Enter command: ").upper()
+
+        if user_input == "HELP":
+            print(
+                """
 Welcome to the help panel, you have the following options:
-            
+                
 Start: To start Car.
-            
+                
 Stop: To stop Car.
-            
+                
 Quit: To exit Car game.
-            """)
+                """)
 
-    elif user_input == "START":
-        if car_on:
-            print("Car is already running")
+        elif user_input == "START":
+            if car_on:
+                print("Car is already running")
+            else:
+                car_on = True
+                print('"VROOOM!" - You have started the Car')
+
+        elif user_input == "STOP":
+            if not car_on:
+                print("Car is already off")
+            else:
+                car_on = False
+                print("*Click* - Car stopped")
+
+        elif user_input == "QUIT":
+            print("Car game ended")
+            game_exit = True
+
         else:
-            car_on = True
-            print('"VROOOM!" - You have started the Car')
+            print("I don't understand that command. They typing 'Help'.")
 
 
-
-    elif user_input == "STOP":
-        if not car_on:
-            print("Car is already off")
-        else:
-            car_on = False
-            print("*Click* - Car stopped")
-
-
-    elif user_input == "QUIT":
-        print("Car game ended")
-        exit = True
-
-    else:
-        print("I don't understand that command. They typing 'Help'.")
+car_game()
